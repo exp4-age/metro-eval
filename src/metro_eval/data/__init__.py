55e12e423ab40fc536b222e2191e5a67c9bb1d19
Adrian Peter Krone <adrian.krone@physik.uni-kassel.de>
2025-11-12 09:58:31 +0100

    Renaming files

--- src/metro_eval/data/__init__.py
+++ src/metro_eval/data/__init__.py
@@ -1,7 +1,8 @@
-from .data import load_all, load_beamtime, load_measurement
+from ._load import load_all, load_beamtime, load_measurement, open_metro_h5
 
 __all__ = [
     "load_all",
     "load_beamtime",
     "load_measurement",
+    "open_metro_h5",
 ]
