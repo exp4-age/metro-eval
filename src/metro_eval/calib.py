5f6886662dba582d70844b5af0bfc3651249e8a8
Adrian Peter Krone <adrian.krone@ptoton.me>
2026-02-26 19:45:56 +0100

    clean up calib

--- src/metro_eval/calib.py
+++ src/metro_eval/calib.py
@@ -8,42 +8,28 @@ if TYPE_CHECKING:
     from numpy.typing import NDArray
 
 
-def pos2wl_converter(
+def wavelength_converter(
     lines_per_mm: int = 1200,
     focal_length: float = 1000.0,
     angle_of_incidence: float = 7.5,
 ) -> callable[[NDArray[np.float64], float, float], NDArray[np.float64]]:
     # angle of incidence 0th order in rad
-    phi = np.deg2rad(angle_of_incidence)
+    φ = np.deg2rad(angle_of_incidence)
 
     # grating constant in nm
     d = 1e6 / lines_per_mm
 
-    def pos2wl(
-        x: NDArray[np.float64], theta: float, scale: float
+    def x_to_λ(
+        x: NDArray[np.float64], θ: float, scale: float
     ) -> NDArray[np.float64]:
-        beta = np.arctan((x - 0.5) * scale / focal_length) + phi + theta
-        return d * (np.sin(phi - theta) + np.sin(beta))
+        β = np.arctan((x - 0.5) * scale / focal_length) + φ + θ
+        return d * (np.sin(φ - θ) + np.sin(β))
 
-    return pos2wl
-
-
-def wl2pos_converter(
-    lines_per_mm: int = 1200,
-    focal_length: float = 1000.0,
-    angle_of_incidence: float = 7.5,
-) -> callable[[NDArray[np.float64], float, float], NDArray[np.float64]]:
-    # angle of incidence 0th order in rad
-    phi = np.deg2rad(angle_of_incidence)
-
-    # grating constant in nm
-    d = 1e6 / lines_per_mm
-
-    def wl2pos(
-        wl: NDArray[np.float64], theta: float, scale: float
+    def λ_to_x(
+        λ: NDArray[np.float64], θ: float, scale: float
     ) -> NDArray[np.float64]:
-        beta = np.arcsin(wl / d - np.sin(phi - theta))
-        x = focal_length * np.tan(beta - (phi + theta))
+        β = np.arcsin(λ / d - np.sin(φ - θ))
+        x = focal_length * np.tan(β - (φ + θ))
         return x / scale + 0.5
 
-    return wl2pos
+    return x_to_λ, λ_to_x
