7274f1122aa50c151c75288195109362e7bd0509
Adrian Peter Krone <adrian.krone@proton.me>
2026-05-07 17:08:22 +0200

    add test for cython modules

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 import numpy
+import os
 
-sorting_tdc = Extension(
-    name="metro_eval.cli.sort_events.sorting_tdc",
-    sources=["src/metro_eval/cli/sort_events/sorting_tdc.pyx"],
-    include_dirs=[numpy.get_include()],
-    optional=True,
-)
+ext_modules = []
 
-index_ascii = Extension(
-    name="metro_eval.cli.metro2hdf._index_ascii",
-    sources=["src/metro_eval/cli/metro2hdf/_index_ascii.py"],
-    optional=True,
-)
+PURE_PYTHON = os.environ.get("PURE_PYTHON") == "1"
 
-setup(ext_modules=cythonize([sorting_tdc, index_ascii]))
+if not PURE_PYTHON:
+    sorting_tdc = Extension(
+        name="metro_eval.cli.sort_events.sorting_tdc",
+        sources=["src/metro_eval/cli/sort_events/sorting_tdc.pyx"],
+        include_dirs=[numpy.get_include()],
+        optional=True,
+    )
+
+    index_ascii = Extension(
+        name="metro_eval.cli.metro2hdf._index_ascii",
+        sources=["src/metro_eval/cli/metro2hdf/_index_ascii.py"],
+        optional=True,
+    )
+
+    ext_modules = cythonize([sorting_tdc, index_ascii])
+
+setup(ext_modules=ext_modules)
